317ddd39c6b001717c2ce9504d83808bd83f2e49
SunLxy <1011771396@qq.com>
2022-04-14 08:08:00 +0800

    feat:新增css和img加载

--- config/webpack.config.study.js
+++ config/webpack.config.study.js
@@ -5,5 +5,20 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.join(process.cwd(), "dist")
+  },
+  module: {
+    rules: [
+      /** 加载 css  */
+      {
+        test: /\.css$/,
+        /** 执行顺序  先执行 `css-loader` 在执行 `style-loader` **/
+        use: ["style-loader", "css-loader"]
+      },
+      /**  加载 图片  */
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './styles/index.css';
+import vcodeIcon from "./assets/vcode.jpg"
 
 function component() {
   const element = document.createElement('div');
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // 添加 class name 名称
+  element.classList.add('hello');
+  // 添加图片到页面
+  const img = new Image(200, 100)
+  img.src = vcodeIcon
+  element.appendChild(img)
   return element;
 }
 
